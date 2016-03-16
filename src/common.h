b2fe55ac7a881eb6169ff20e5dd5b290bc74c941
Dylan Allbee <dylan@allbee.org>
2016-03-16 16:36:23 -0700

    Added power_of_two function to common.h

--- src/common.h
+++ src/common.h
@@ -4,3 +4,13 @@
 #define MAX(a,b) (((a)>(b))?(a):(b))
 
 
+size_t next_power_of_2(size_t n)
+{
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    n++;
+}
