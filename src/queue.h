79c1f0d4c8b0eddad8547e96c12713c842da821d
Dylan Allbee <dylan@allbee.org>
2016-03-23 15:53:37 -0700

    Updated queue header to reflect generic typing

--- src/queue.h
+++ src/queue.h
@@ -6,12 +6,13 @@ typedef struct queue_t queue;
 
 struct queue_t {
   void **array;
-  int *front;
-  int *back;
+  void **front;
+  void **back;
   size_t size;
 };
 
 void *queue_front(queue *q);
+size_t queue_length(queue *q);
 void queue_init(queue *q);
 void queue_push(queue *q, void *e);
 void queue_pop(queue *q);
