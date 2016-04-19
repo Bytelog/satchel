0592029a694172e4a5627f0436fe555db561b5e9
Dylan Allbee <dylan@allbee.org>
2016-04-19 08:18:25 -0700

    Moves to single .h file approach

--- src/queue.h
+++ src/queue.h
@@ -1,7 +1,7 @@
 #pragma once
-
-#include "common.h"
 #include <stdbool.h>
+#include <string.h>
+#include "common.h"
 
 typedef struct queue_t queue;
 
@@ -12,8 +12,35 @@ struct queue_t {
   size_t count;
 };
 
-void const *queue_front(queue *q);
-void queue_init(queue *q);
-bool queue_push(queue *q, void const *e);
-void queue_pop(queue *q);
-void queue_free(queue *q);
+void const *queue_front(queue *q) { return q->array[q->front]; }
+
+void queue_init(queue *q) {
+  q->array = NULL;
+  q->size = 0;
+  q->front = 0;
+  q->count = 0;
+}
+
+static inline void *queue_grow(queue *q) {
+  q->size = MAX(next_pow2(q->size + 1), 2);
+  q->array = realloc(q->array, sizeof(void *) * q->size);
+  return q->array;
+}
+
+bool queue_push(queue *q, void const *e) {
+  if (q->count == q->size) {
+    if (!queue_grow(q)) return false;
+    memmove(&q->array[q->count], q->array, q->front);
+  }
+
+  q->count++;
+  q->array[(q->front + q->count - 1) % q->size] = e;
+  return true;
+}
+
+void queue_pop(queue *q) {
+  q->count--;
+  q->front = (q->front + 1) % q->size;
+}
+
+void queue_free(queue *q) { free(q->array); }
--- src/vec.h
+++ src/vec.h
@@ -1,6 +1,8 @@
 #pragma once
-#include <stdlib.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include "common.h"
 
 typedef struct vec_t vec;
 
@@ -10,11 +12,48 @@ struct vec_t {
   size_t count;
 };
 
-void const *vec_get(vec *v, size_t n);
-void vec_init(vec *v);
-bool vec_push(vec *v, void const *);
-void vec_pop(vec *v);
-bool vec_insert(vec *v, size_t pos, void const *);
-void vec_erase(vec *v, size_t pos);
-void vec_clear(vec *v);
-void vec_free(vec *v);
+void const *vec_get(vec *v, size_t n) { return v->array[n]; }
+
+void vec_init(vec *v) {
+  v->array = NULL;
+  v->size = 0;
+  v->count = 0;
+}
+
+static inline void *vec_grow(vec *v) {
+  if (v->count == v->size) {
+    v->size = next_pow2(v->size + 1);
+    v->array = realloc(v->array, sizeof(void *) * v->size);
+  }
+  return v->array;
+}
+
+bool vec_push(vec *v, void const *e) {
+  if (!vec_grow(v)) return false;
+  v->array[v->count++] = e;
+  return true;
+}
+
+void vec_pop(vec *v) { v->count--; }
+
+bool vec_insert(vec *v, size_t pos, void const *val) {
+  if (!vec_grow(v)) return false;
+  memmove(&v->array[pos + 1], &v->array[pos],
+          sizeof(void *) * (v->count - pos));
+  v->array[pos] = val;
+  v->count++;
+  return true;
+}
+
+void vec_erase(vec *v, size_t pos) {
+  v->count--;
+  memmove(&v->array[pos], &v->array[pos + 1],
+          sizeof(void *) * (v->count - pos));
+}
+
+void vec_clear(vec *v) { v->count = 0; }
+
+void vec_free(vec *v) {
+  free(v->array);
+  v->array = NULL;
+}
