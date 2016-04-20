dc9062888ac590b6c95469d6ce8f7f408bfe7590
Dylan Allbee <dylan@allbee.org>
2016-04-20 14:48:39 -0700

    Added iterator support to list

--- src/collections/list.h
+++ src/collections/list.h
@@ -5,6 +5,7 @@
 
 typedef struct list_node list_node;
 typedef struct list list;
+typedef struct list_it list_it;
 
 struct list_node {
   void *data;
@@ -13,25 +14,43 @@ struct list_node {
 
 struct list {
   list_node *first;
-  list_node *last;
 };
 
-void *list_front(list *l) { return l->first->data; }
-void *list_back(list *l) { return l->last->data; }
-void *list_get(list_node *n) { return n->data; }
-size_t list_count(list *l) { return (l->last - l->first) + 1; }
+struct list_it {
+  list_node *item;
+};
+
+void list_front(list *l, list_it *it) { it->item = l->first; }
+void *list_get(list *l, list_it *it) { return it->item; }
+void list_advance(list *l, list_it *it) { it->item = it->item->next; }
 
 bool list_init(list *l) {
   if (!(l->first = malloc(sizeof(list_node))))
     return false;
 
-  l->last = l->first;
   l->first->data = NULL;
   l->first->next = NULL;
   return true;
 }
 
-list_node *list_node_next(list_node *n) { return n->next; }
+bool list_insert_after(list *l, list_it *it, void *e) {
+  list_node *head;
+  if (!(head = malloc(sizeof(list_node))))
+    return false;
+
+  head->data = e;
+  head->next = it->item->next;
+  return true;
+}
+
+void list_erase_after(list *l, list_it *it) {
+  if (!it->item->next)
+    return;
+
+  list_node *trash = it->item->next;
+  it->item->next = it->item->next->next;
+  free(trash);
+}
 
 bool list_push_front(list *l, void *e) {
   list_node *tail;
@@ -49,27 +68,10 @@ void list_pop_front(list *l) {
   free(trash);
 }
 
-bool list_insert_after(list_node *n, void *e) {
-  list_node *head;
-  if (!(head = malloc(sizeof(list_node))))
-    return false;
-
-  head->data = e;
-  head->next = n->next;
-  return true;
-}
-
-void list_erase_after(list_node *n) {
-  if (!n->next)
-    return;
-
-  list_node *trash = n->next;
-  n->next = n->next->next;
-  free(trash);
-}
-
 void list_free(list *l) {
+  list_it it;
+  list_front(l, &it);
   while(l->first->next)
-    list_erase_after(l->first);
+    list_erase_after(l, &it);
   free(l->first);
 }
