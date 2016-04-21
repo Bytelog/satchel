26f977d650589647bc9c125bce03bf6eafedccfc
Dylan Allbee <dylan@allbee.org>
2016-04-21 14:49:28 -0700

    Updates stack test to use size_t

--- test/stack.c
+++ test/stack.c
@@ -2,20 +2,20 @@
 #include <assert.h>
 #include <stdint.h>
 
-#define FROM_INT void *)(intptr_t
-#define TO_INT int)(intptr_t const *
+#define FROM_UINT void *)(uintptr_t
+#define TO_UINT size_t)(uintptr_t const *
 
 int main() {
   stack s;
   stack_init(&s);
 
-  for (int i = 0; i < 100; ++i) {
-    stack_push(&s, (FROM_INT)i);
-    assert((TO_INT)stack_top(&s) == i);
+  for (size_t i = 0; i < 100; ++i) {
+    stack_push(&s, (FROM_UINT)i);
+    assert((TO_UINT)stack_top(&s) == i);
   }
 
-  for (int i = 99; 0 <= i; --i) {
-    assert((TO_INT)stack_top(&s) == i);
+  for (size_t i = 99; 0 != i; --i) {
+    assert((TO_UINT)stack_top(&s) == i);
     stack_pop(&s);
   }
 
