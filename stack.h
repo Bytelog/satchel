927a3b4e7d02784f6b874d47dc2fc29364e4ef48
Dylan Allbee <dylan@allbee.org>
2016-04-21 15:41:45 -0700

    Updated stack with count() and added unit testing

--- stack.h
+++ stack.h
@@ -3,7 +3,8 @@
 
 typedef struct vec stack;
 
-inline void const *stack_top(stack *s) { return vec_get(s, s->count - 1); }
+inline void const *stack_top(stack *s) { return vec_get(s, vec_count(s) - 1); }
+inline size_t stack_count(stack *s) { return vec_count(s); }
 inline void stack_init(stack *s) { vec_init(s); }
 inline bool stack_push(stack *s, void *e) { return vec_push(s, e); }
 inline void stack_pop(stack *s) { vec_pop(s); }
--- test/stack.c
+++ test/stack.c
@@ -1,21 +1,36 @@
 #include "stack.h"
 #include <assert.h>
+#include <limits.h>
 #include <stdint.h>
 
 #define FROM_UINT void *)(uintptr_t
 #define TO_UINT size_t)(uintptr_t const *
+#define STRESS_VALUE UINT_MAX
 
 int main() {
   stack s;
   stack_init(&s);
 
-  for (size_t i = 0; i < 100; ++i) {
-    stack_push(&s, (FROM_UINT)i);
-    assert((TO_UINT)stack_top(&s) == i);
-  }
+  // Basic usage tests
+  assert(stack_count(&s) == 0);
 
-  for (size_t i = 99; 0 != i; --i) {
-    assert((TO_UINT)stack_top(&s) == i);
+  stack_push(&s, (FROM_UINT) 10);
+  assert((TO_UINT) stack_top(&s) == 10);
+  assert(stack_count(&s) == 1);
+
+  stack_pop(&s);
+  assert(stack_count(&s) == 0);
+
+  stack_free(&s);
+
+
+  // Stress testing
+  stack_init(&s);
+  for (size_t i = 0; i < STRESS_VALUE; ++i)
+    stack_push(&s, (FROM_UINT) i);
+
+  for (size_t i = STRESS_VALUE - 1; 0 < i; --i) {
+    assert((TO_UINT) stack_top(&s) == i);
     stack_pop(&s);
   }
 
